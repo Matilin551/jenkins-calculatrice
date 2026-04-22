8ad8ea15a44a7db4d7a3649ac4a11b71a9c3e5ec
matilin.ollivier <matilin551@gmail.com>
2026-04-22 16:02:05 +0200

    fix script

--- calculatrice-app.sh
+++ calculatrice-app.sh
@@ -2,6 +2,9 @@
 
 echo "********************************"
 echo "CONSTRUCTION DE L'APPLICATION SUR LE PORT 3000"
+
+export DOCKER_BUILDKIT=0
+export COMPOSE_DOCKER_CLI_BUILD=0
 docker-compose up -d --build
 echo "********************************"
 date
